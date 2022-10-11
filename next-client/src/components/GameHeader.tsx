988ca313a2d25e20d37a46b2d1bf2ecdba0df3fa
Craig Dempsey <craigdempsey@Craigs-MacBook-Pro.local>
2022-10-11 14:42:11 -0400

    format code

--- next-client/src/components/GameHeader.tsx
+++ next-client/src/components/GameHeader.tsx
@@ -1,23 +1,26 @@
-import { useStoreActions, useStoreState } from "../store/store"
+import { useStoreActions, useStoreState } from "../store/store";
 import Button from "@mui/material/Button";
-import SwapCallsRoundedIcon from '@mui/icons-material/SwapCallsRounded';
-
-
+import SwapCallsRoundedIcon from "@mui/icons-material/SwapCallsRounded";
 
 const GameHeader = () => {
-  const movieTitle = useStoreState(state => state.movies.currentGameMovie?.original_title);
-  const setGameOn = useStoreActions(actions => actions.setGameOn );
+  const movieTitle = useStoreState(
+    (state) => state.movies.currentGameMovie?.original_title
+  );
+  const setGameOn = useStoreActions((actions) => actions.setGameOn);
   const handleMovieSwap = () => {
     setGameOn(false);
-  }
+  };
 
   return (
     <>
-      <h2>{`${movieTitle}`}<Button
-        onClick={handleMovieSwap}
-      ><SwapCallsRoundedIcon /></Button></h2>
+      <h2>
+        {`${movieTitle}`}
+        <Button onClick={handleMovieSwap}>
+          <SwapCallsRoundedIcon />
+        </Button>
+      </h2>
     </>
-  )
-}
+  );
+};
 
 export default GameHeader;
